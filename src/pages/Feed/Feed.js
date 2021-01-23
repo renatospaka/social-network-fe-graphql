0eaf415bcd68ffbad733461bcd50a94e38174621
Renato Spakauskas <renatospaka@gmail.com>
2021-01-23 17:01:19 -0300

    feat: :sparkles: get all posts

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -54,26 +54,49 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8091/feed/posts?page=' + page, {
+
+    const gqlQuery = {
+      query: `
+        {
+          getPosts {
+            posts {
+              _id, 
+              title, 
+              content
+              creator {
+                name
+              }
+              createdAt
+            }
+            totalPosts
+          }
+        }
+      `
+    };
+
+    fetch('http://localhost:8091/graphql', {
+      method: 'POST',  
+      body: JSON.stringify(gqlQuery),
       headers: {
-        Authorization: 'Bearer ' + this.props.token
+        Authorization: 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json'
       }
     })
       .then(res => {
-        if (res.status !== 200) {
-          throw new Error('Failed to fetch posts.');
-        }
         return res.json();
       })
-      .then(resData => {
+      .then(resData => { 
+        if (resData.errors) {
+          throw new Error('Fetching posts failed.');
+        }
         this.setState({
-          posts: resData.posts.map(post => {
+          posts: resData.data.getPosts.posts.map(post => {
             return {
               ...post,
               imagePath: post.imageUrl
             };
           }),
-          totalPosts: resData.totalItems,
+          totalPosts: resData.data.getPosts.totalPosts,
           postsLoading: false
         });
       })
@@ -182,7 +205,17 @@ class Feed extends Component {
           createdAt: resData.data.createPost.createdAt
         };
         this.setState(prevState => {
+          let updatedPosts = [...prevState.posts];
+          if (prevState.editPost) {
+            const postIndex = prevState.posts.findIndex(
+              p => p._id === prevState.editPost._id
+            );
+            updatedPosts[postIndex] = post;
+          } else {
+            updatedPosts.unshift(post);
+          }
           return {
+            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
